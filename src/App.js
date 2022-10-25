9d2133a0cff185d89eabd8389d36f1489643ad11
Taqinur <taqi1662@gmail.com>
2022-10-25 17:29:17 +0600

    added title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes/Routes"
 
 function App() {
   return (
     <div>
+      <RouterProvider router= {routes}></RouterProvider>
     </div>
   );
 }
