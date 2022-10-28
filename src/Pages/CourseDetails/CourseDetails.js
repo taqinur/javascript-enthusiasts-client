ea1d5ac3df230006aa76f073f11155b51f09a378
Taqinur <taqi1662@gmail.com>
2022-10-28 18:09:23 +0600

    course details page done

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const CourseDetails = () => {
+    const course = useLoaderData();
+    const {title, price, description, category, image} = course;
+
     return (
-        <div>
-            det
+        <div className="m-auto mt-5 card mb-3" style={{ maxWidth: '40rem', maxHeight: '40rem'}} >
+        <div className="row g-0">
+            <div className="col-md-4 my-auto">
+                <img src={image} className="img-fluid rounded-start" alt="..."/>
+            </div>
+            <div className="col-md-8">
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <small className='text-danger'>Difficulty level: {category}</small>
+                <p className="card-text">{description}</p>
+                <h3 className="card-text text-info">Price: BDT {price}</h3>
+                <Button variant="outline-dark"><Link>Get Premium Access</Link></Button>
+            </div>
+            </div>
+        </div>
         </div>
     );
 };
